4da7c8d0322c13591b55c2b2d34bf3bef9f1b38f
salamiselim <mrtentacion01@gmail.com>
2025-07-07 16:33:01 +0100

    initial commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,37 @@
 interface ContractsConfig {
     [chainId: number]: {
-        AirdropSender: string
+        tsender: string
         no_check: string | null
     }
 }
 
-export const chainsToTSender: ContractsConfig = {
+    export const chainsToTSender: ContractsConfig = {
+    324: {
+        tsender: "0x7e645Ea4386deb2E9e510D805461aA12db83fb5E",
+        no_check: null,
+    },
+    1: {
+        tsender: "0x3aD9F29AB266E4828450B33df7a9B9D7355Cd821",
+        no_check: "0x7D4a746Cb398e5aE19f6cBDC08473664ADBc6da5",
+    },
+    42161: {
+        tsender: "0xA2b5aEDF7EEF6469AB9cBD99DE24a6881702Eb19",
+        no_check: "0x091bAB6497F2Cc429c82c5807Df4faA34235Cccc",
+    },
+    10: {
+        tsender: "0xAaf523DF9455cC7B6ca5637D01624BC00a5e9fAa",
+        no_check: "0xa0c7ADA2c7c29729d12e2649BC6a0a293Ac46725",
+    },
+    8453: {
+        tsender: "0x31801c3e09708549c1b2c9E1CFbF001399a1B9fa",
+        no_check: "0x39338138414Df90EC67dC2EE046ab78BcD4F56D9",
+    },
+    31337: {
+        tsender: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        no_check: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    },
     11155111: {
-        AirdropSender: "0x7823282b2f2E6dB9568c71Dc7f34016b071A3c0d",
+        tsender: "0x7823282b2f2E6dB9568c71Dc7f34016b071A3c0d",
         no_check: "0x23c531Cc71a437b53Ad17751C614c07b0d929bD3",
     }
 }
--- src/rainbowkitConfig.tsx
+++ src/rainbowkitConfig.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import {sepolia} from "wagmi/chains"
+import {sepolia, mainnet, anvil} from "wagmi/chains"
 
 export default getDefaultConfig({
     appName: "Airdrop Sender",
     projectId: "82721db4d10b78e9ec1eb4de7f0f983b",
-    chains: [sepolia],
+    chains: [sepolia, mainnet, anvil],
     ssr: false
 })
