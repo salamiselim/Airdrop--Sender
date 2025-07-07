de08bccd60aec5b1a5a737656500ae3f91b3b409
salamiselim <salami_olomu@DESKTOP-R5KS7CJ.localdomain>
2025-07-07 15:59:23 +0100

    initial commit

--- src/rainbowkitConfig.tsx
+++ src/rainbowkitConfig.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import {anvil, zksync, mainnet} from "wagmi/chains"
+import {sepolia} from "wagmi/chains"
 
 export default getDefaultConfig({
     appName: "Airdrop Sender",
     projectId: "82721db4d10b78e9ec1eb4de7f0f983b",
-    chains: [anvil, zksync, mainnet],
+    chains: [sepolia],
     ssr: false
 })
